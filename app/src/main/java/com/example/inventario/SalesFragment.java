7215e42abd9a23e37518aebe125635371a3a51c4
Kureikode <kureikode@gmail.com>
2023-10-22 22:00:25 -0500

    .

--- app/src/main/java/com/example/inventario/SalesFragment.java
+++ app/src/main/java/com/example/inventario/SalesFragment.java
@@ -1,95 +1,73 @@
 package com.example.inventario;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SalesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SalesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText editText;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private SharedPreferences sharedPreferences;
 
     public SalesFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SalesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SalesFragment newInstance(String param1, String param2) {
-        SalesFragment fragment = new SalesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        getContentView(R.layout.fragment_sales);
+
+        editText = getView.findViewById(R.id.editText);
+        listView = findViewById(R.id.listView);
+
+        sharedPreferences = getSharedPreferences("HistorialPrefs", Context.MODE_PRIVATE);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+    }
+
+
+
+    public void guardarEnHistorial(View view) {
+        String nuevoTexto = editText.getText().toString().trim();
+
+        if (!nuevoTexto.isEmpty()) {
+
+            String historialActual = sharedPreferences.getString("historial", "");
+
+
+            String nuevoHistorial = historialActual + "\n" + nuevoTexto;
+
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("historial", nuevoHistorial);
+            editor.apply();
+
+
+            adapter.add(nuevoTexto);
+            editText.setText("");
         }
     }
-
-    private Switch switchSales;
-    private TextView textAltas;
-    private TextView textHighValue;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_sales, container, false);
-        return view;
+    private void cargarYMostrarHistorial() {
+        String historial = sharedPreferences.getString("historial", "");
+        String[] historialArray = historial.split("\n");
+        adapter.clear();
+        adapter.addAll(historialArray);
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        switchSales = view.findViewById(R.id.switchSales);
-        textAltas = view.findViewById(R.id.textAltas);
-        textHighValue = view.findViewById(R.id.textHighValue);
-
-        switchSales.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    textAltas.setVisibility(View.VISIBLE);
-                    textHighValue.setVisibility(View.VISIBLE);
-                } else {
-                    textAltas.setVisibility(View.GONE);
-                    textHighValue.setVisibility(View.GONE);
-                }
-            }
-        });
+    public void onResume() {
+        super.onResume();
+        cargarYMostrarHistorial();
     }
 }
