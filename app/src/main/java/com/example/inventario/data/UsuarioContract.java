194153ed4b86fe4afc3c53933cf57bb088422d66
Mozaaar <141757001+Mozaaar@users.noreply.github.com>
2023-10-24 10:16:54 -0500

    Lista para usuarios, usuario adapter

--- app/src/main/java/com/example/inventario/data/UsuarioContract.java
+++ app/src/main/java/com/example/inventario/data/UsuarioContract.java
@@ -7,6 +7,7 @@ public class UsuarioContract {
         public static final String TABLE_NAME ="usuarios";
         public static final String ID  ="codigoUsuario";
         public static final String NAME  ="nomUsuario";
+        public static final String LASTNAME = "apUsuario";
         public static final String USER  ="usuario";
         public static final String PASSWORD ="password";
 
